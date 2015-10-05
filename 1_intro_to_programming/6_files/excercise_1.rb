510b23f487f4ffe50e5a08cec9c05e9ba4eeeb0d
Wesley Potratz <wes.potratz@gmail.com>
2015-10-05 21:34:42 +0000

    1. Intro to Programming - 6. Files

---
+++ 1_intro_to_programming/6_files/excercise_1.rb
@@ -0,0 +1,11 @@
+
+
+simple = File.read("simple_file.txt")
+
+original = File.new("original_file.txt", "w+")
+
+File.open(original, "a") do |file|
+  file.puts simple
+end
+
+puts File.read(original)
