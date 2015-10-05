9656345856ffafb4599408d052bbc8b409aaa679
Wesley Potratz <wes.potratz@gmail.com>
2015-10-05 21:24:19 +0000

    1. Intro to Programming 4. Arrays

---
+++ 1_intro_to_programming/4_arrays/array_num_check.rb
@@ -0,0 +1,7 @@
+
+
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+puts arr.include?(number)
---
+++ 1_intro_to_programming/4_arrays/build_new_array.rb
@@ -0,0 +1,10 @@
+
+
+a = [1, 2, 3, 4, 5]
+
+b = a.map {|n| n + 2}
+
+puts a
+puts b
+puts a.to_s
+puts b.to_s
